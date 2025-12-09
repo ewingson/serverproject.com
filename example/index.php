324c374d3f667fd74e2a5b8c7bb379744ea6e38d
Matthias Evering <me@evering.eu>
2025-12-09 14:46:23 +0100

    adressierung

--- example/index.php
+++ example/index.php
@@ -9,13 +9,13 @@
 <p>hier k&ouml;nnte 'hello world' stehen...</p>
 <a href = 'https://selfhtml.org/'>hier selfhtml at https://selfhtml.org/</a><br />
 <!--erste adressierungsart-->
-<a href = 'https://www.serverproject.de/etc/freax/files/'>/etc/freax/files/ {([one])}</a><br />
+<a href = 'https://www.serverproject.de/example/etc/freax/files/'>./etc/freax/files/ {([one])}</a><br />
 <!--zweite adressierungsart-->
-<a href = '/etc/freax/files/'>/etc/freax/files/ {([two])}</a><br />
+<a href = '/example/etc/freax/files/'>./etc/freax/files/ {([two])}</a><br />
 <!--dritte adressierungsart-->
-<a href = './files/'>/etc/freax/files/ {([three])}</a><br />
+<a href = './etc/freax/files/'>./etc/freax/files/ {([three])}</a><br />
 <!--vierte adressierungsart-->
-<a href = 'files/'>/etc/freax/files/ {([four])}</a><br />
+<a href = 'etc/freax/files/'>./etc/freax/files/ {([four])}</a><br />
 <a href = '../draft/'>back</a><br />
 <hr /><br />
 <?php
